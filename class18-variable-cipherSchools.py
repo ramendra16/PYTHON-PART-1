d0d85f66f10bf3fc2a0b228031e09a757ca85880
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-12-01 22:24:55 +0530

    python file

---
+++ class18-variable-cipherSchools.py
@@ -0,0 +1,13 @@
+number1=2
+print(number1)
+#string,number
+name="yash"
+print(name)
+name=123
+print(name)
+#rule for variables
+#1number=4
+#letter,_.................first letter
+_name="yash"
+user_one_name="yash"   #snake case writing
+userOneName="ankit"  #camel case writing
