e4a7fa48ec3cae5625bd806a233d04749ee502c6
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-12-01 22:23:25 +0530

    python file

---
+++ class17-calculator-cipherSchools.py
@@ -0,0 +1,22 @@
+print(2+3)
+print(2+3*4)
+#floor division-gives floating point division
+print(2/4)
+#integer division
+print(4//2)
+print(2//4)
+#exponent
+print(2**3)
+print(2**0.5)
+print(round(2**0.5,4))
+print(2**3/2*6-4*(3-4/2))
+print((2+3)/2)
+print(3%2)
+print((2+3)*5/2%6)
+#5*5/2%6
+#25/2%6
+print(12.5%6)
+#exponents
+print(2**3**2)
+#(2**3)
+print(2**9)
